f01c9f51b6c4792202897c820fa42381ee90291c
Sayed <sayedhossainsifat300@gmail.com>
2021-12-22 23:33:12 +0600

    done

---
+++ letConst.js
@@ -0,0 +1,20 @@
+const hubby = "Sayeada Jothi";
+// console.log(hubby);
+
+const numbers = [23,52,63];
+numbers[2] = 100 ;
+numbers.push(200);
+numbers.pop();
+// console.log(numbers);
+
+let patientName = "Rahim Chaca"; // let is a scope variable 
+patientName = "Karim Dewan";
+patientName = "Asad Molla";
+patientName = "Monzila Prama";
+console.log(patientName);
+
+let sum= 0 
+for (let i = 0; i <10; i++) {
+    sum = sum + i;
+    console.log(i)
+};
