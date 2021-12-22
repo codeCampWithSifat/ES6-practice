c8dfb6f542101f3ffa53b49ff3eeaea5d0fd7ce6
Sayed <sayedhossainsifat300@gmail.com>
2021-12-23 00:32:59 +0600

    alhamdullaiha

---
+++ arrow.js
@@ -0,0 +1,21 @@
+// // function doubleIt (num) {
+// //     return num * 2 ;
+// // } 
+// // const result = doubleIt(20);
+// // console.log(result);
+
+// const doubleIt = function myFun(num) {
+//     return num * 2 ;
+// } 
+// const result = doubleIt(20);
+// console.log(result);
+
+const doubleIt = (num) => {
+    return num*2
+};
+const result = doubleIt(20);
+console.log(result);
+
+const add = (x,y) => x + y ;
+const sum  = add(5,3);
+console.log(sum);
