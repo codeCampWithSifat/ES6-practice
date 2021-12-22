c2dd0a94e5a4a2325b45afed04b5d2bb77b191a1
Sayed <sayedhossainsifat300@gmail.com>
2021-12-23 00:17:55 +0600

    done

---
+++ template.js
@@ -0,0 +1,14 @@
+const firstName = "Sifat";
+const lastName = "Sayed";
+const fullName = firstName +" "+ lastName +" is a good boy";
+const fullName2 = `${firstName} ${lastName}  ${20+30} is a good boy`
+console.log(fullName);
+console.log(fullName2);
+
+const multiline = "i love you \n"
++"i miss you\n";
+    console.log(multiline);
+const multiline2 = `I love you
+I need You
+I miss you`;
+console.log(multiline2)
